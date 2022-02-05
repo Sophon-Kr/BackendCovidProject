a31f0df1bf1a409e2baef333a7869293e8a29d77
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-02-05 21:28:12 +0700

    for deploy

--- index.js
+++ index.js
@@ -14,8 +14,18 @@ const databaseConfig = DatabaseConfig();
 const test = require("./routers/test");
 const sendMail = require("./routers/sendMail");
 
-const serve = app.listen(5000, () => {
-  console.log("Server listening on ", 5000);
+var port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  var responseObject = {
+    status: true,
+    data: { text: "hello" },
+  };
+  res.json(responseObject);
+});
+
+const serve = app.listen(port, () => {
+  console.log("Server listening on ", port);
 });
 
 try {
